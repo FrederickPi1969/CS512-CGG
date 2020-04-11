f6ecc155ac94537cf248561775424095f01616fe
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2020-04-11 01:36:16 -0500

    Update graph_operations.py
    fixed bugs related to sorting

--- graph_operations.py
+++ graph_operations.py
@@ -23,55 +23,52 @@ def self_repetition(G, n = 1, linknode = 0):
     return new_graph
 
 # densify via triad closing
-def densify(G, n = 1, target_density = 1):
-    centerlist = [(nx.clustering(G, i), i) for i in G.nodes()].sort()
-    while nx.transitivity(G) < max(target_density, 1):
+def densify(G, target_density = 1):
+    centerlist = sorted([(nx.clustering(G, i), i) for i in G.nodes()], reverse = True)
+    while nx.transitivity(G) < min(target_density, 1):
         while centerlist[0][0] == 1:
             centerlist.pop(0)
         candidates = combinations(list(G.nodes()), r = 2) - G.edges()
-        G.add_edge(*[(G.degree[i] * G.degree[j], (i, j)) for (i, j) in candidates].sort()[0][1])
-        centerlist = [(nx.clustering(G, j), j) for (i, j) in centerlist].sort()
+        G.add_edge(*sorted([(G.degree[i] * G.degree[j], (i, j)) for (i, j) in candidates])[0][1])
+        centerlist = sorted([(nx.clustering(G, j), j) for (i, j) in centerlist], reverse = True)
 
 # densify via triad breaking
-def sparsify(G, n = 1, target_density = 0):
-    centerlist = [(nx.clustering(G, i), i) for i in G.nodes()].sort(reverse = True)
-    while nx.transitivity(G) > max(target_density, 1):
+def sparsify(G, target_density = 0):
+    centerlist = sorted([(nx.clustering(G, i), i) for i in G.nodes()])
+    while nx.transitivity(G) > max(target_density, 0):
         while centerlist[0][0] == 0:
             centerlist.pop(0)
-        candidates = intersection(set(combinations(list(G.nodes()), r = 2)), set(G.edges()))
-        G.remove_edge(*[(max(G.degree[i], G.degree[j]) - min(G.degree[i], G.degree[j]), (i, j)) for (i, j) in candidates].sort()[0][1])
-        centerlist = [(nx.clustering(G, j), j) for (i, j) in centerlist].sort()
+        candidates = set(combinations(list(G.nodes()), r = 2)).intersection(set(G.edges()))
+        G.remove_edge(*sorted([(max(G.degree[i], G.degree[j]) - min(G.degree[i], G.degree[j]), (i, j)) for (i, j) in candidates])[0][1])
+        centerlist = sorted([(nx.clustering(G, j), j) for (i, j) in centerlist])
 
-def add_edge_coherent(G, n = 1, rev = True):
-    for i in range(n):
-        edgelist = [(G.degree[i] * G.degree[j], (i, j)) for (i, j) in G.edges()].sort(reverse = rev)
-        for j in range(min(m, len(edgelist))):
-            G.remove_edge(*edgelist[j][1])
+def add_edge_coherent(G, n = 1, descending = True):
+    edgelist = sorted([(G.degree[i] * G.degree[j], (i, j)) for (i, j) in combinations(list(G.nodes()), r = 2) - G.edges()], reverse = descending)
+    for j in range(min(n, len(edgelist))):
+        G.add_edge(*edgelist[j][1])
 
-def remove_edge_coherent(G, n = 1, rev = True):
-    for i in range(n):
-        edgelist = [(G.degree[i] * G.degree[j], (i, j)) for (i, j) in G.edges()].sort(reverse = rev)
-        for j in range(min(m, len(edgelist))):
-            G.remove_edge(*edgelist[j][1])
+def remove_edge_coherent(G, n = 1, descending = False):
+    edgelist = sorted([(G.degree[i] * G.degree[j], (i, j)) for (i, j) in G.edges()], reverse = descending)
+    for j in range(min(n, len(edgelist))):
+        G.remove_edge(*edgelist[j][1])
 
 def remove_edge_difference(G, n = 1):
-    for i in range(n):
-        edgelist = [(max(G.degree[i], G.degree[j]) - min(G.degree[i], G.degree[j]), (i, j)) for (i, j) in G.edges()].sort()
-        for j in range(min(m, len(edgelist))):
-            G.remove_edge(*edgelist[j][1])
+    edgelist = sorted([(max(G.degree[i], G.degree[j]) - min(G.degree[i], G.degree[j]), (i, j)) for (i, j) in G.edges()])
+    for j in range(min(n, len(edgelist))):
+        G.remove_edge(*edgelist[j][1])
         
 
-def add_node(G, namelist = None, n = 1, m = 1):
+def add_node(G, namelist = None, n = 1, m = 1, descending = True):
     namelist = namelist if namelist else [len(G) + i for i in range(n)]
     for i in range(min(n, len(namelist))):
-        nodelist = [(G.degree[i], i) for i in G.nodes()].sort()
+        nodelist = sorted([(G.degree[i], i) for i in G.nodes()], reverse = descending)
         G.add_node(namelist[i])
         for j in range(min(m, len(nodelist))):
             G.add_edge(namelist[i], nodelist[j][1])
 
 
-def remove_node(G, n = 1, weighted = True):
-    for i in range(n):
-        nodelist = [(G.degree[i], i) for i in G.nodes()].sort()
-        for j in range(min(m, len(nodelist))):
-            G.remove_node(nodelist[j][1])
+def remove_node(G, n = 1, descending = False):
+    nodelist = sorted([(G.degree[i], i) for i in G.nodes()], reverse = descending)
+    print(nodelist)
+    for j in range(min(n, len(nodelist))):
+        G.remove_node(nodelist[j][1])
