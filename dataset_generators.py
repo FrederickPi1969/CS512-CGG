bacf37bfbe8725ebcdd43703e1abd62bfe83b152
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2020-04-11 02:04:48 -0500

    Update dataset_generators.py
    fixed infinite loop in subgraph sampler

--- dataset_generators.py
+++ dataset_generators.py
@@ -3,29 +3,32 @@ import networkx as nx
 import string
 import random
 import queue
-from itertools import combinations
+from itertools import permutations
 
-def sample_subgraph(G, start = None, prob = 0.8):
+def sample_subgraph(G, prob = 0.8, start = None):
     subgraph = nx.null_graph()
     initial_node = start
     if not start or start not in G.nodes():
-        initial_node = np.random.choice(list(G.keys()))
+        initial_node = np.random.choice(list(G.nodes()))
     
-    node_queue = Queue()
-    node_queue.put((initial_node), prob)
+    node_queue = queue.Queue()
+    node_queue.put((initial_node, prob))
+    seen = set()
+    seen.add(initial_node)
 
     ## sample nodes
     while not node_queue.empty():
-        new_node = node_queue.pop()
+        new_node = node_queue.get()
         subgraph.add_node(new_node[0])
-        for neighbor in G.neighbors(new_node[0]):
-            if random.random() > new_node[1]:
-              node_queue.put((neighbor, new_node[1] * prob))
+        for neighbor in set(G.neighbors(new_node[0])) - seen:
+            roll = random.random()
+            seen.add(neighbor)
+            if roll < new_node[1]:
+                node_queue.put((neighbor, new_node[1] * prob))
 
     ## add edges
-    for node in subgraph.nodes():
-        for neighbor in list(intersection(set(G.neighbors(node)), set(subgraph.nodes()))):
-            subgraph.add_edge(neighbor, node)
+    for edge in set(permutations(list(subgraph.nodes()), 2)).intersection(set(G.edges())):
+        subgraph.add_edge(*edge)
 
     return subgraph
 
