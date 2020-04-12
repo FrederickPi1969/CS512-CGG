7614deb6416af639c6d17c788624c37d7d5372c4
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2020-04-12 03:17:30 -0500

    Update dataset_generators.py
    added generator for trees

--- dataset_generators.py
+++ dataset_generators.py
@@ -5,6 +5,174 @@ import random
 import queue
 from itertools import permutations
 
+# Python code to insert a node in AVL tree 
+  
+# Generic tree node class 
+class TreeNode(object): 
+    def __init__(self, val): 
+        self.val = val 
+        self.left = None
+        self.right = None
+        self.height = 1
+  
+# AVL tree class which supports the  
+# Insert operation 
+class AVL_Tree(object): 
+  
+    # Recursive function to insert key in  
+    # subtree rooted with node and returns 
+    # new root of subtree. 
+    def insert(self, root, key): 
+      
+        # Step 1 - Perform normal BST 
+        if not root: 
+            return TreeNode(key) 
+        elif key < root.val: 
+            root.left = self.insert(root.left, key) 
+        else: 
+            root.right = self.insert(root.right, key) 
+  
+        # Step 2 - Update the height of the  
+        # ancestor node 
+        root.height = 1 + max(self.getHeight(root.left), 
+                           self.getHeight(root.right)) 
+  
+        # Step 3 - Get the balance factor 
+        balance = self.getBalance(root) 
+  
+        # Step 4 - If the node is unbalanced,  
+        # then try out the 4 cases 
+        # Case 1 - Left Left 
+        if balance > 1 and key < root.left.val: 
+            return self.rightRotate(root) 
+  
+        # Case 2 - Right Right 
+        if balance < -1 and key > root.right.val: 
+            return self.leftRotate(root) 
+  
+        # Case 3 - Left Right 
+        if balance > 1 and key > root.left.val: 
+            root.left = self.leftRotate(root.left) 
+            return self.rightRotate(root) 
+  
+        # Case 4 - Right Left 
+        if balance < -1 and key < root.right.val: 
+            root.right = self.rightRotate(root.right) 
+            return self.leftRotate(root) 
+  
+        return root 
+  
+    def leftRotate(self, z): 
+  
+        y = z.right 
+        T2 = y.left 
+  
+        # Perform rotation 
+        y.left = z 
+        z.right = T2 
+  
+        # Update heights 
+        z.height = 1 + max(self.getHeight(z.left), 
+                         self.getHeight(z.right)) 
+        y.height = 1 + max(self.getHeight(y.left), 
+                         self.getHeight(y.right)) 
+  
+        # Return the new root 
+        return y 
+  
+    def rightRotate(self, z): 
+  
+        y = z.left 
+        T3 = y.right 
+  
+        # Perform rotation 
+        y.right = z 
+        z.left = T3 
+  
+        # Update heights 
+        z.height = 1 + max(self.getHeight(z.left), 
+                        self.getHeight(z.right)) 
+        y.height = 1 + max(self.getHeight(y.left), 
+                        self.getHeight(y.right)) 
+  
+        # Return the new root 
+        return y 
+  
+    def getHeight(self, root): 
+        if not root: 
+            return 0
+  
+        return root.height 
+  
+    def getBalance(self, root): 
+        if not root: 
+            return 0
+  
+        return self.getHeight(root.left) - self.getHeight(root.right) 
+  
+    def preOrder(self, root): 
+  
+        if not root: 
+            return
+  
+        print("{0} ".format(root.val), end="") 
+        self.preOrder(root.left) 
+        self.preOrder(root.right) 
+
+class BST_Tree(object): 
+  
+    def insert(self, root, key): 
+      
+        # Step 1 - Perform normal BST 
+        if not root: 
+            return TreeNode(key) 
+        elif key < root.val: 
+            root.left = self.insert(root.left, key) 
+        else: 
+            root.right = self.insert(root.right, key) 
+  
+        return root 
+
+def generate_trees(n = 20):
+    sequence = np.random.permutation(n)
+
+    a = AVL_Tree()
+    b = BST_Tree()
+    a_root = None
+    b_root = None
+  
+    for i in sequence:
+        a_root = a.insert(a_root, i) 
+        b_root = a.insert(b_root, i) 
+
+    avl_graph = nx.null_graph()
+    avl_queue = queue.Queue()
+    avl_queue.put(a_root)
+    print(a_root.val)
+    while not avl_queue.empty():
+        current = avl_queue.get()
+        if current.left:
+            avl_graph.add_edge(current.val, current.left.val)
+            avl_queue.put(current.left)
+        if current.right:
+            avl_graph.add_edge(current.val, current.right.val)
+            avl_queue.put(current.right)
+
+    bst_graph = nx.null_graph()
+    bst_queue = queue.Queue()
+    bst_queue.put(b_root)
+    print(b_root.val)
+    while not bst_queue.empty():
+        current = bst_queue.get()
+        if current.left:
+            bst_graph.add_edge(current.val, current.left.val)
+            bst_queue.put(current.left)
+        if current.right:
+            bst_graph.add_edge(current.val, current.right.val)
+            bst_queue.put(current.right)
+
+    return (avl_graph, bst_graph)
+
 def dblp():
     name_dict = {}
     nodes = open('authorDict.txt', 'r').readlines()
