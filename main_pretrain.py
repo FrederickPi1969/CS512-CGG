corrected masked normalization

main_pretrain.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     node_attributes = "degree" #@param ["uniform", "degree", "random"]
     dataArgs["node_attr"] = node_attributes
 
-    number_of_graph_instances = "1500" #@param [1, 100, 1000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
+    number_of_graph_instances = "2048" #@param [1, 100, 1000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
     dataArgs["n_graph"] = int(number_of_graph_instances)
 
     dataArgs["upper_triangular"] = False
@@ -129,6 +129,11 @@ if __name__ == "__main__":
     batched_A_hat_min_test = []
     print("\n\n =================Extracting useful information=====================")
     vae.eval()
+
+    def index_of(my_list, target):
+        try: return my_list.index(target)
+        except: return dataArgs["max_n_node"]
+
     for e in range(1):
         loss_cum = 0
         for i in range(len(Attr_train)):
@@ -149,11 +154,23 @@ if __name__ == "__main__":
                 discretizer = Discretizer(A_discretize, A_hat_discretize)
                 A_hat_discretize = discretizer.discretize('hard_threshold')
                 A_hat_discretize = torch.unsqueeze(torch.from_numpy(A_hat_discretize), -1)
+
                 batched_A_hat_discretized.append(A_hat_discretize)
                 batched_A_hat_raw_train.append(A_hat_raw.detach())
                 batched_A_hat_max_train.append(max_score_per_node.detach())
                 batched_A_hat_min_train.append(min_score_per_node.detach())
 
+                # count = 0
+                for j in range(len(batched_A_hat_discretized[i])):
+                    temp = torch.diag(batched_A_hat_discretized[i][j].detach().reshape(dataArgs["max_n_node"], -1))
+                    pred_node_num = index_of(list(temp), 0)
+                    Param_train[i][j][-1] = pred_node_num  # predicting node num have ~85% acc
+                    # true_node_num = int(Param_train[i][j][0])
+                    # print(pred_node_num)
+                    # print(true_node_num)
+                #
+                #     count += pred_node_num == true_node_num
+                # print(f"node prediction accuracy : {count / len(batched_A_hat_discretized[i])}")
 
             loss = loss_func((A, attr), (A_hat, attr_hat), z_mean, z_log_var, trainArgs, modelArgs)
             loss_cum += loss.item()
@@ -286,7 +303,7 @@ if __name__ == "__main__":
     discriminator.eval()
     ## operation = "transitivity", "density", "node_count"
     transform = GraphTransform(dataArgs["max_n_node"], operation = "forest_fire", sigmoid = True)
-    w_epochs = 1  ### adjust epoch here!!!
+    w_epochs = 10  ### adjust epoch here!!!
 
     loss_train = []
     w_A_train = []

utils.py
@@ -681,7 +681,8 @@ def generate_batched_mask(batched_A_hat, batched_param):
     batch_size, max_node, _ = batched_A_hat.shape
     mask = torch.zeros(batch_size, max_node, max_node)
     for i in range(batch_size):
-        n = int(batched_param[i][0])
+        print(batched_param[i][-1])
+        n = int(batched_param[i][-1])  # dim -1 is the predicted node num
         mask[i][:n,:n] = torch.ones([n,n])
     return mask  # -> (b, n, n)
 

visualization.py
@@ -352,7 +352,8 @@ def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen
 		gen_A_normal = torch.stack(gen_A_normal).cpu().squeeze().numpy()
 		masked_normalized_A = torch.stack(masked_normalized_A).squeeze().numpy()
 
-		discretizer = Discretizer(gen_A_normal, gen_A_normal)
+		# discretizer = Discretizer(gen_A_normal, gen_A_normal)
+		discretizer = Discretizer(masked_normalized_A, masked_normalized_A) ## Changed this to masked_normalized_A_hat
 		gen_A_discretize = discretizer.discretize(discretize_method)
 		dump_list[-1] = gen_A_discretize
 
@@ -376,15 +377,15 @@ def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen
 			print('edit_a:')
 			print(edit_a)
 			print('gen_A_AAT_raw:')
-			print(a_raw)
-			print('max_A:')
-			print(max_A)
-			print('min_A:')
-			print(min_A)
+			# print(a_raw)
+			# print('max_A:')
+			# print(max_A)
+			# print('min_A:')
+			# print(min_A)
 			print("masked_normalized_A:")
 			print(masked_norm_A)
-			print('gen_A_normalized:')
-			print(a_normal)
+			# print('gen_A_normalized:')
+			# print(a_normal)
 			print('gen_A_discretize:')
 			print(a)
 			print('=====')