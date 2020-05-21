corrected masked normalization

main_pretrain.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     node_attributes = "degree" #@param ["uniform", "degree", "random"]
     dataArgs["node_attr"] = node_attributes
 
-    number_of_graph_instances = "15000" #@param [1, 100, 1000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
+    number_of_graph_instances = "1500" #@param [1, 100, 1000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
     dataArgs["n_graph"] = int(number_of_graph_instances)
 
     dataArgs["upper_triangular"] = False
@@ -83,6 +83,7 @@ if __name__ == "__main__":
     Attr_test = generate_batch(torch.from_numpy(Attr[int((1-trainArgs["data_split"])*Attr.shape[0]):]), trainArgs["batch_size"])
     Param_test = generate_batch(torch.from_numpy(Param[int((1-trainArgs["data_split"])*Param.shape[0]):]), trainArgs["batch_size"])
     Topol_test = generate_batch(torch.from_numpy(Topol[int((1-trainArgs["data_split"])*Topol.shape[0]):]), trainArgs["batch_size"])
+
 
     # print(A_train.shape)
     # print(len(Attr_train), Attr_train[0].shape)
@@ -285,7 +286,7 @@ if __name__ == "__main__":
     discriminator.eval()
     ## operation = "transitivity", "density", "node_count"
     transform = GraphTransform(dataArgs["max_n_node"], operation = "forest_fire", sigmoid = True)
-    w_epochs = 35  ### adjust epoch here!!!
+    w_epochs = 1  ### adjust epoch here!!!
 
     loss_train = []
     w_A_train = []
@@ -295,6 +296,7 @@ if __name__ == "__main__":
     gen_A_raw_train = []
     gen_A_max_train = []
     gen_A_min_train = []
+    masked_norm_A_hats = []
 
     recent_train_losses = []
 
@@ -350,6 +352,8 @@ if __name__ == "__main__":
                 w_edit_A_hat_train.append(edit_A)
                 w_gen_A_hat_train.append(gen_A.detach())
                 gen_A_raw_train.append(gen_A_raw.detach())
+                masked_norm_A = masked_normalization(gen_A_raw.detach(), Param_train[i])
+                masked_norm_A_hats.append(masked_norm_A)
                 gen_A_max_train.append(gen_A_max.detach())
                 gen_A_min_train.append(gen_A_min.detach())
 
@@ -358,7 +362,7 @@ if __name__ == "__main__":
 
         loss_train.append(loss_cum / len(batched_A_hat))
 
-    debugDiscretizer(w_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen_A_min_train, w_gen_A_hat_train, discretize_method="hard_threshold", printMatrix=True, abortPickle=True)
+    debugDiscretizer(w_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen_A_min_train, w_gen_A_hat_train, masked_norm_A_hats, discretize_method="hard_threshold", printMatrix=True, abortPickle=True)
     #debugDecoder(w_edit_A_hat_train, [], w_gen_A_hat_train, [], discretize_method="hard_threshold", printMatrix=True)
     # drawGraph(w_A_train, w_A_hat_train, w_edit_A_hat_train, w_gen_A_hat_train)
     # drawGraphSaveFigure(w_A_train, w_A_hat_train, w_edit_A_hat_train, w_gen_A_hat_train, clearImage=True)

utils.py
@@ -681,8 +681,8 @@ def generate_batched_mask(batched_A_hat, batched_param):
     batch_size, max_node, _ = batched_A_hat.shape
     mask = torch.zeros(batch_size, max_node, max_node)
     for i in range(batch_size):
-        n = batched_param[i][0]
-        mask[i][:n,:n] = torch.ones(n,n)
+        n = int(batched_param[i][0])
+        mask[i][:n,:n] = torch.ones([n,n])
     return mask  # -> (b, n, n)
 
 def masked_normalization(batched_A_hat, batched_param):

visualization.py
@@ -319,9 +319,10 @@ To generate useful log information, set the 'printMatrix' flag to True and run t
 command:
 	python3 main.py > somePath/steering_gan_log.txt
 """
-def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen_A_min_train, w_gen_A_hat_train, discretize_method="hard_threshold", printMatrix=True, abortPickle=False):
+def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen_A_min_train, w_gen_A_hat_train,
+					 masked_norm_A_hats, discretize_method="hard_threshold", printMatrix=True, abortPickle=False):
 	# check pickle
-	gen_A, edit_A, gen_A_max, gen_A_min, gen_A_normal, gen_A_discretize = [], [], [], [], [], None
+	gen_A, edit_A, gen_A_max, gen_A_min, gen_A_normal, gen_A_discretize, masked_normalized_A = [], [], [], [], [], None,[]
 	dump_list = [gen_A, edit_A, gen_A_max, gen_A_min, gen_A_normal, gen_A_discretize]
 	file_list = ['gen_A', 'edit_A', 'gen_A_max', 'gen_A_min', 'gen_A_normal', 'gen_A_discretize']
 	if not os.path.exists('pickles'):
@@ -341,12 +342,15 @@ def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen
 			gen_A_max.extend(gen_A_max_train[i])
 			gen_A_min.extend(gen_A_min_train[i])
 			gen_A_normal.extend(w_gen_A_hat_train[i])
+			masked_normalized_A.extend(masked_norm_A_hats[i])
+
 
 		gen_A = torch.stack(gen_A).squeeze().cpu().numpy()
 		edit_A = torch.stack(edit_A).squeeze().numpy()
 		gen_A_max = torch.stack(gen_A_max).cpu().squeeze().numpy()
 		gen_A_min = torch.stack(gen_A_min).cpu().squeeze().numpy()
 		gen_A_normal = torch.stack(gen_A_normal).cpu().squeeze().numpy()
+		masked_normalized_A = torch.stack(masked_normalized_A).squeeze().numpy()
 
 		discretizer = Discretizer(gen_A_normal, gen_A_normal)
 		gen_A_discretize = discretizer.discretize(discretize_method)
@@ -364,7 +368,9 @@ def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen
 		edit_a = edit_A[i]
 		max_A = gen_A_max[i]
 		min_A = gen_A_min[i]
+		masked_norm_A = masked_normalized_A[i]
 		a_normal = gen_A_normal[i]
+
 		if printMatrix:	
 			print('=====')
 			print('edit_a:')
@@ -375,6 +381,8 @@ def debugDiscretizer(gen_edit_A_hat_train, gen_A_raw_train, gen_A_max_train, gen
 			print(max_A)
 			print('min_A:')
 			print(min_A)
+			print("masked_normalized_A:")
+			print(masked_norm_A)
 			print('gen_A_normalized:')
 			print(a_normal)
 			print('gen_A_discretize:')